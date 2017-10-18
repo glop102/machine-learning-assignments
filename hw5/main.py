02e7c810cb964822d29d76ebd3b208273463fab9
glop102 <alexlikeschess@gmail.com>
2017-10-18 13:21:15 -0400

    sortof working resnet?

--- hw5/main.py
+++ hw5/main.py
@@ -81,50 +81,74 @@ dropout_keeprate = tf.placeholder(tf.float32)
 
 def convolutionUnit(node,shape): #kernel size x&y, features in&out
 	w = tf.Variable(tf.truncated_normal(shape, stddev=0.1))
+	#w = tf.nn.l2_normalize(w,0)
 	b = tf.Variable(tf.constant(0.1, shape=[shape[-1]]))
 	conv = tf.nn.convolution(node,w,"SAME") + b
 	return conv
 
+# def residualSection(node,features1,features2):
+# 	n1 = convolutionUnit(tf.nn.relu(node),[1,1,features2,features1]) #1x1 convoluion
+# 	n1 = tf.nn.dropout(n1,dropout_keeprate)
+
+# 	n2a = convolutionUnit(tf.nn.relu(n1),[1,3,features1,features1]) #vertical convolution
+# 	n2b = convolutionUnit(tf.nn.relu(n1),[3,1,features1,features1]) #horizontal convolution
+# 	n2 = tf.concat((n2a,n2b),axis=3)
+# 	#n2 = tf.nn.dropout(n2,dropout_keeprate)
+
+# 	n3 = convolutionUnit(tf.nn.relu(n2),[1,1,features1*2,features2]) #1x1 convoluion
+# 	#n3 = tf.nn.dropout(n3,dropout_keeprate)
+
+# 	#pool = tf.nn.max_pool(n3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+# 	return tf.nn.l2_normalize(n3 + node , 3)
+# def residualSection(node,features1,features2):
+# 	n1 = convolutionUnit(tf.nn.relu(node),[1,1,features2,features1]) #1x1 convoluion
+# 	n1 = tf.nn.dropout(n1,dropout_keeprate)
+# 	#n1 = tf.nn.l2_normalize(n1,0)
+
+# 	n2 = convolutionUnit(tf.nn.relu(n1),[3,3,features1,features1]) #vertical convolution
+# 	#n2 = tf.nn.dropout(n2,dropout_keeprate)
+# 	#n2 = tf.nn.l2_normalize(n2,0)
+
+# 	n3 = convolutionUnit(tf.nn.relu(n2),[1,1,features1,features2]) #1x1 convoluion
+# 	n3 = tf.nn.dropout(n3,dropout_keeprate)
+# 	#n3 = tf.nn.l2_normalize(n3,0)
+
+# 	return n3 + node
 def residualSection(node,features1,features2):
-	n1 = convolutionUnit(tf.nn.relu(node),[1,1,features2,features1]) #1x1 convoluion
-	#n1 = tf.nn.dropout(n1,dropout_keeprate)
+	n1 = convolutionUnit(tf.nn.relu(node),[3,3,features2,features1]) #1x1 convoluion
+	n2 = convolutionUnit(tf.nn.relu(n1),[3,3,features1,features1]) #vertical convolution
 
-	n2a = convolutionUnit(tf.nn.relu(n1),[1,3,features1,features1]) #vertical convolution
-	n2b = convolutionUnit(tf.nn.relu(n1),[3,1,features1,features1]) #horizontal convolution
-	n2 = tf.concat((n2a,n2b),axis=3)
-	#n2 = tf.nn.dropout(n2,dropout_keeprate)
-
-	n3 = convolutionUnit(tf.nn.relu(n2),[1,1,features1*2,features2]) #1x1 convoluion
-	#n3 = tf.nn.dropout(n3,dropout_keeprate)
-
-	#pool = tf.nn.max_pool(n3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-	return tf.nn.l2_normalize(n3,0) + node
+	return tf.nn.l2_normalize(n2 + node , 0)
 
 def denseUnit(node,shape):
 	w = tf.Variable(tf.truncated_normal(shape, stddev=0.1))
 	b = tf.Variable(tf.constant(0.1, shape=[shape[-1]]))
-	return tf.nn.relu(tf.matmul(node,w)+b)
+	x = tf.nn.relu(tf.matmul(node,w)+b)
+	#x = tf.nn.l2_normalize(x,0)
+	return x
 
-l1 = tf.nn.relu(convolutionUnit(input_node,[3,3,3,20]))
-l1 = tf.nn.relu(convolutionUnit(l1,[3,3,20,50]))
+l1 = tf.nn.relu(convolutionUnit(input_node,[3,3,3,32]))
+l1 = tf.nn.relu(convolutionUnit(l1,[3,3,32,64]))
 for _ in range(2):
-	l1 = residualSection(l1,30,50)
-l2 = tf.nn.max_pool( convolutionUnit(l1,[3,3,50,125]) , [1,2,2,1],[1,2,2,1],"SAME") #16x16
+	l1 = residualSection(l1,64,64)
+l2 = tf.nn.max_pool( convolutionUnit(l1,[3,3,64,128]) , [1,2,2,1],[1,2,2,1],"SAME") #16x16
+# l2 = tf.nn.l2_normalize(l2,0)
 for _ in range(2):
-	l2 = residualSection(l2,75,125)
-l3 = tf.nn.max_pool( convolutionUnit(l2,[3,3,125,250]) , [1,2,2,1],[1,2,2,1],"SAME") #8x8
+	l2 = residualSection(l2,128,128)
+l3 = tf.nn.max_pool( convolutionUnit(l2,[3,3,128,256]) , [1,2,2,1],[1,2,2,1],"SAME") #8x8
+# l3 = tf.nn.l2_normalize(l3,0)
 for _ in range(2):
-	l3 = residualSection(l3,125,250)
-l4 = convolutionUnit(l3,[3,3,250,600])
+	l3 = residualSection(l3,256,256)
+l4 = convolutionUnit(l3,[3,3,256,512])
+# l4 = tf.nn.l2_normalize(l4,0)
 for _ in range(2):
-	l4 = residualSection(l4,450,600)
+	l4 = residualSection(l4,512,512)
 l5 = tf.nn.max_pool(l4,[1,2,2,1],[1,2,2,1],"SAME") #4x4
-l5 = tf.reshape(l5,[-1,4*4*600])
+l5 = tf.reshape(l5,[-1,4*4*512])
 l5 = tf.nn.dropout(l5,dropout_keeprate)
+# l5 = tf.nn.l2_normalize(l5,0)
 
-l6 = denseUnit(l5,[4*4*600,2048])
-#l6 = tf.nn.dropout(l6,dropout_keeprate)
-l6 = denseUnit(l6,[2048,1024])
+l6 = denseUnit(l5,[4*4*512,1024])
 #l6 = tf.nn.dropout(l6,dropout_keeprate)
 l6 = denseUnit(l6,[1024,512])
 l6 = tf.nn.dropout(l6,dropout_keeprate)
@@ -147,8 +171,8 @@ optimizer = tf.train.AdamOptimizer().minimize(cross_entropy)
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-batch_size = 50
-max_epochs = 6000
+batch_size = 75
+max_epochs = 10000
 drop_keep = 0.75
 for epoch in range(max_epochs+1):
 	if(epoch%100 == 0):
