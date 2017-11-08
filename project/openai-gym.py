ecedf2b575ec1c06e4275f9e926c52027f406f8c
glop102 <alexlikeschess@gmail.com>
2017-11-08 15:09:17 -0500

    found an example working DeepQ

--- project/openai-gym.py
+++ project/openai-gym.py
@@ -9,11 +9,11 @@ from collections import deque
 env = gym.make("CartPole-v0")
 env.reset()
 
-memory_size = 1000
+memory_size = 10000
 memory = deque(maxlen=memory_size)
 
 y = 0.95 #discount rate for future gains - higher makes it favor future more
-e = 0.1 #chance of forcing a random action
+e = 1.0 #chance of forcing a random action
 
 def denseUnit_custom(node,shape):
 	w = tf.Variable(tf.random_normal(shape, stddev=0.1,dtype=tf.float32))
@@ -43,83 +43,35 @@ loss = tf.reduce_sum(tf.square(wanted_output - output_values),axis=1)
 sess = tf.Session()
 curt_state = env.reset()
 step_counter = 0
-reward_total = 0
 actions_taken=[]
-prevAction = 0
-numPrevAction = 0
+epoch_counter = 0
 
 # trainer = tf.train.AdamOptimizer()
 trainer = tf.train.GradientDescentOptimizer(0.0001)
 optimize = trainer.minimize(loss)
 sess.run(tf.global_variables_initializer())
-sequence = []
 
-def predictWholeSequence(sess,e):
-	sequence = []
-	actions_taken = []
-	reward_total = 0
-	curt_state = env.reset()
-	step_counter = 0
-	while True:
-		env.render()
-		step_counter += 1
-		#Chose an action for the current state
-		action,outputs = sess.run([output_action,output_values],feed_dict={input_state:[curt_state]})
-		if np.random.rand(1) < e:
-			action[0] = env.action_space.sample()
-		actions_taken.append(action[0])
-
-		#see what the action does
-		next_state, reward, done, info = env.step(action[0])
-		reward_total += reward
-		#and what the network wants to do for the new state
-		outputs_next = sess.run(output_values,feed_dict={input_state:[next_state]})
-
-		sequence.append({
-			"curt_state":curt_state,
-			"next_state":next_state,
-			"reward":reward,
-			"reward_total":reward_total,
-			"action":action[0],
-			"done":done
-			})
-		curt_state = next_state
-		print(outputs[0])
-		if done == True:
-			# Reduce chance of random action as we train the model.
-			# e = 1./((step_counter/50) + 10)
-			line = ""
-			for x in actions_taken : line += str(x)
-			print(line)
-			actions_taken=[]
-			step_counter = 0
-			return sequence
-def trainNet(sess,seq):
-	max_reward = seq[-1]["reward_total"] / 10
-	prevAction = 0
-	numPrevAction = 0
+def trainNet():
+	seq = np.random.choice(memory,size=100)
+	states=[]
+	actions=[]
+	wanted=[]
 	for step in seq:
 		curt_state = step["curt_state"]
-		reward = step["reward"]
-		action = step["action"]
-		if action != prevAction:
-			prevAction = action
-			numPrevAction = 0
+		next_state = step["next_state"]
+		reward     = step["reward"]
+		action     = step["action"]
 		outputs      = sess.run(output_values,feed_dict={input_state:[curt_state]})
-		outputs_next = sess.run(output_values,feed_dict={input_state:[step["next_state"]]})
+		outputs_next = sess.run(output_values,feed_dict={input_state:[next_state]})
 
-		# delta = reward + ( y*np.max(outputs_next))
-		# delta = max_reward + ( y*np.max(outputs_next)) - numPrevAction
-		# delta = max_reward + y*( np.max(outputs_next) ) - (numPrevAction)
-		# target_outputs = np.array( [[0]*len(outputs_next[0])] ,dtype=np.float32)
+		delta = reward + ( y*np.max(outputs_next))
 		target_outputs = outputs.copy()
 		target_outputs[0,action] = delta
 
-		sess.run(optimize,feed_dict={input_state:[curt_state],wanted_output:target_outputs})
+		states.append(curt_state)
+		wanted.append(target_outputs[0])
 
-# while True:
-# 	seq = predictWholeSequence(sess,e)
-# 	trainNet(sess,seq)
+	sess.run(optimize,feed_dict={input_state:states,wanted_output:wanted})
 
 while True:
 	env.render()
@@ -129,66 +81,41 @@ while True:
 	if np.random.rand(1) < e:
 		action[0] = env.action_space.sample()
 	actions_taken.append(action[0])
-	if action[0] != prevAction:
-		prevAction = action[0]
-		numPrevAction = 0
 
 	#see what the action does
 	next_state, reward, done, info = env.step(action[0])
-	reward_total += reward
-	#and what the network wants to do for the new state
-	outputs_next = sess.run(output_values,feed_dict={input_state:[next_state]})
 
-	# how much do we want to reinforce the action taken?
-	delta = reward + ( y*np.max(outputs_next))
-	# delta = reward + y*( np.max(outputs_next)-np.min(outputs_next) ) - (numPrevAction**2)
-	# delta = reward + y*( np.max(outputs_next)-np.min(outputs_next) ) - (numPrevAction)
-	# delta = reward + ( y*np.max(outputs_next)) - ( y*np.min(outputs_next)) - (numPrevAction**2)
-	# delta = reward + ( y*np.max(outputs_next)) - (numPrevAction**2)
-	# delta = reward_total + y*np.max(outputs_next)
-	# delta = y*np.max(outputs_next)
-	# delta = outputs[0,action[0]]
-	# if done:
-	# 	delta = reward_total
-	# put the amount to reinforce as the only delta of the output to have it train
-	target_outputs = outputs.copy()
-	# target_outputs = np.array( [[0]*len(outputs[0])] ,dtype=np.float32)
-	# target_outputs[0,action[0]] = delta
-	target_outputs[0,action[0]] += delta
-	# target_outputs[0,action[0]] = 1
-	print(action,outputs,target_outputs)
+	memory.append({
+		"curt_state":curt_state,
+		"next_state":next_state,
+		"reward":reward,
+		"action":action,
+		"done":done
+		})
+
+	trainNet()
 
 	curt_state = next_state
 	if done == True:
 		curt_state = env.reset()
 		reward_total = 0
 		# Reduce chance of random action as we train the model.
-		e = 1./((step_counter/50) + 10)
+		# e = 1./((step_counter/50) + 10)
 		# e -= 0.001
-		# e *= 0.999
+		e *= 0.998
 
 		#pretty little progres bar of how long it runs before being "done"
 		# print((" "*int(step_counter)) + "#")
 		line = ""
 		for x in actions_taken : line += str(x)
-		print(line)
+		print(epoch_counter,"{:.4f}".format(e),line)
 		actions_taken=[]
 		step_counter = 0
-		memory.append(sequence)
+		epoch_counter += 1
 		# break
 
-	sess.run(optimize,feed_dict={input_state:[curt_state],wanted_output:target_outputs})
+	# sess.run(optimize,feed_dict={input_state:[curt_state],wanted_output:target_outputs})
 
 	# print(next_state)
 	# print(env.action_space)
 	# print(reward)
-
-
-
-#TODO LIST
-#little NN stuff above for DeepQ
-#clean room - mostly bags and bottles
-#clean room - move food around
-#NN stuff again
-#laundry
-#take check to bank
